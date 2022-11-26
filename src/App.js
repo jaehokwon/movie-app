cbf851e9f4b953f80adefa5cd7680ad1348bfdcb
hotel9uest <kk4417jh@gmail.com>
2022-11-26 14:21:31 +0900

    feat: react basic(4:make todo list)

--- src/App.js
+++ src/App.js
@@ -1,52 +1,35 @@
 import { useEffect, useState } from "react";
 
-function Hello() {
-  useEffect(() => {
-    console.log("created :)");
-    return () => console.log("destroyed :(");
-  }, []);
-  return <h1>Hello</h1>;
-}
-
-function CleanUpTest() {
-  const [showing, setShowing] = useState(true);
-  const onClick = () => setShowing((current) => !current);
-  return (
-    <div>
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
-      {showing ? <Hello /> : null}
-    </div>
-  );
-}
-
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log("CALL THE API");
-  }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword or counter changes.");
-  }, [keyword, counter]);
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const onChange = (ev) => setTodo(ev.target.value);
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    if (todo === "") {
+      return;
+    }
+    setTodoList((current) => [todo, ...current]);
+    setTodo("");
+  };
   return (
     <div>
-      <CleanUpTest />
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      ></input>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      <h1>My TO-DOs ({todoList.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={todo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To-Do</button>
+      </form>
+      <hr />
+      <ul>
+        {todoList.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
