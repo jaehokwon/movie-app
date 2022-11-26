725d398147ff901d40bfaf538f6882a943c3358e
hotel9uest <kk4417jh@gmail.com>
2022-11-26 15:23:32 +0900

    feat: react basic(5: coin tracker)

--- src/App.js
+++ src/App.js
@@ -1,35 +1,73 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todoList, setTodoList] = useState([]);
-  const onChange = (ev) => setTodo(ev.target.value);
-  const onSubmit = (ev) => {
-    ev.preventDefault();
-    if (todo === "") {
-      return;
-    }
-    setTodoList((current) => [todo, ...current]);
-    setTodo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [index, setIndex] = useState("none");
+  const [amount, setAmount] = useState(0);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((data) => {
+        setCoins(data);
+        setLoading(false);
+      });
+  }, []);
+  const onSelectChange = (ev) => {
+    setIndex(ev.target.value);
+  };
+  const onInputChange = (ev) => {
+    setAmount(ev.target.value);
   };
   return (
     <div>
-      <h1>My TO-DOs ({todoList.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={todo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add To-Do</button>
-      </form>
+      {loading ? null : (
+        <div>
+          <h1>Coin Converter</h1>
+          <select onChange={onSelectChange} value={index}>
+            <option value="none">Select Dollor to Coin</option>
+            {coins.map((coin, index) => (
+              <option key={coin.id} value={index}>
+                {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+          {index === "none" ? null : (
+            <div>
+              <div>
+                <label htmlFor="usd">USD</label>
+                <input
+                  id="usd"
+                  type="number"
+                  value={amount}
+                  onChange={onInputChange}
+                />
+              </div>
+              <div>
+                <label htmlFor="coin">{coins[index].name}</label>
+                <input
+                  id="coin"
+                  type="number"
+                  value={amount / coins[index].quotes.USD.price}
+                />
+              </div>
+            </div>
+          )}
+        </div>
+      )}
       <hr />
-      <ul>
-        {todoList.map((todo, index) => (
-          <li key={index}>{todo}</li>
-        ))}
-      </ul>
+      <h1>The Coins!{loading ? "" : `(${coins.length})`}</h1>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <ul>
+          {coins.map((coin) => (
+            <li key={coin.id}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
